db3604fb34de6a8207558ce09507c1310e0754a0
PauloJose26 <paulojoliveira.2616@gmail.com>
2022-01-19 19:50:38 -0300

    Layout do Login terminado

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -7,82 +7,93 @@ import {
   InputLeftElement,
   InputGroup,
 } from "@chakra-ui/react";
-import { useState, useEffect, useCallback, useRef } from "react";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  ForwardRefRenderFunction,
+  forwardRef,
+} from "react";
 import { FieldError } from "react-hook-form";
 import { IconType } from "react-icons";
 
 interface InputProps extends ChakraInputProps {
-    name: string;
-    label?: string;
-    error?: FieldError | null;
-    icon?: IconType;
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  icon?: IconType;
 }
 
 type inputVariationProps = {
-    [key: string]: string
+  [key: string]: string;
 };
+
 const InputVariation: inputVariationProps = {
-    error: "red.600",
-    default: "gray.300",
-    focus: "purple.800",
-    filled: "green.600"
+  error: "red.600",
+  default: "gray.300",
+  focus: "purple.800",
+  filled: "green.600",
 };
 
-export const Input = ({ name, label, error = null, icon: Icon, ...rest }: InputProps) => {
-    const [variation, setVariation] = useState("default");
+const BaseInput: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, icon: Icon, ...rest },
+  ref
+) => {
+  const [value, setValue] = useState<string>("");
+  const [variation, setVariation] = useState<string>("default");
 
-    const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (error) {
+      setVariation("error");
+    }
+  }, [error]);
 
-    useEffect(() => {
-        if(error){
-            setVariation("error");
-        }
-    }, [error]);
+  const handleInputFocus = useCallback(() => {
+    if (!error) {
+      setVariation("focus");
+    }
+  }, [error]);
 
-    const handleInputFocus = useCallback(() => {
-        if(!error){
-            setVariation("focus");
-        }
-    }, [error]);
+  const handleInputBlur = useCallback(() => {
+    if (value.length > 1 && !error) {
+      setVariation("filled");
+    }
+  }, [error, value]);
 
-    const handleInputBlur = useCallback(() => {
-        if(inputRef.current?.value && !error){
-            setVariation("filled");
-        }
-    }, [error]);
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel>{label}</FormLabel>}
 
-    return(
-        <FormControl>
-            {!!label && (
-                <FormLabel>{ label }</FormLabel>
-            )}
+      <InputGroup flexDirection="column">
+        {Icon && (
+          <InputLeftElement color={InputVariation[variation]} mt="5px">
+            <Icon />
+          </InputLeftElement>
+        )}
 
-            <InputGroup flexDirection="column">
-                {Icon && (
-                    <InputLeftElement color={ InputVariation[variation] } mt="2.5">
-                        <Icon />
-                    </InputLeftElement>
-                )}
+        <ChakraInput
+          name={name}
+          bg="gray.50"
+          variant="outline"
+          color={InputVariation[variation]}
+          borderColor={InputVariation[variation]}
+          _hover={{ bgColor: "gray.100" }}
+          _placeholder={{ color: "gray.300" }}
+          size="lg"
+          h="50px"
+          onFocus={handleInputFocus}
+          onBlurCapture={handleInputBlur}
+          onChangeCapture={(e) => setValue(e.currentTarget.value)}
+          {...rest}
+          ref={ref}
+        />
 
-                <ChakraInput 
-                    name={ name } 
-                    bg="gray.50" 
-                    variant="outline"
-                    color={ InputVariation[variation] }
-                    borderColor={ InputVariation[variation] }
-                    _hover={{ bgColor: "gray.100" }}
-                    _placeholder={{ color: "gray.300" }}
-                    size="lg"
-                    h="60px"
-                    onFocus={ handleInputFocus }
-                    onBlurCapture={ handleInputBlur }
-                    { ...rest } 
-                />
-
-                {!!error && (
-                    <FormErrorMessage>{ error.message }</FormErrorMessage>
-                ) }
-            </InputGroup>
-        </FormControl>
-    );
+        {!!error && (
+            <FormErrorMessage>{error.message}</FormErrorMessage>
+        )}
+      </InputGroup>
+    </FormControl>
+  );
 };
+
+export const Input = forwardRef(BaseInput);
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,39 @@
-import { Flex, Grid, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+
 import { FaEnvelope, FaLock } from "react-icons/fa";
 
 import LogoPrimary from "../../assets/logo-primary.svg";
 import { Input } from "../../components/Form/Input";
 
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
 export const Login = () => {
+  const [ loading, setLoading ] = useState(false);
+
+  const loginSchema = yup.object().shape({
+    email: yup.string().required("Campo obrigatório").email("E-mail inválido"),
+    password: yup.string().required("Campo Obrigatório")
+  });
+
   const {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm();
+  } = useForm<LoginData>({
+    resolver: yupResolver(loginSchema)
+  });
+
+  const handleLogin = (data: LoginData) => {
+  };
 
   return (
     <Flex
@@ -18,11 +41,11 @@ export const Login = () => {
       color="white"
       padding="10px 15px"
       h="100vh"
-      bgGradient="linear(to-r, purple.700 60%, white 40%)"
+      bgGradient="linear(to-r, purple.800 65%, white 35%)"
       w="100%"
     >
       <Flex w="100%" flexDirection="row" justifyContent="center" align="center">
-        <Grid w="50%" paddingRight="100px">
+        <Grid w="100%" paddingRight="100px">
           <Image src={LogoPrimary} alt="doit" boxSize="120px" />
           <Heading as="h1">O jeito fácil, grátis</Heading>
           <Text>
@@ -32,6 +55,8 @@ export const Login = () => {
         </Grid>
         <Grid
           as="form"
+          onSubmit={ handleSubmit(handleLogin) }
+          action="post"
           mt="4"
           w="50%"
           padding="30px 15px"
@@ -39,11 +64,56 @@ export const Login = () => {
           borderColor="gray.100"
           bg="white"
           color="gray.900"
+          autoComplete="off"
         >
           <Heading size="lg">Bem vindo de volta!</Heading>
           <VStack marginTop="6" spacing="5">
-            <Input placeholder="Digite seu Login" icon={ FaEnvelope } { ...register("word") } />
-            <Input placeholder="Digite sua Senha" icon={ FaLock } { ...register("password") } />
+            <Box w="100%">
+              <Input 
+                placeholder="Digite seu Login"
+                label="Login"
+                type="email"
+                error={ errors.email }
+                icon={ FaEnvelope }
+                { ...register("email") } 
+              />
+              { !errors.email && <Text ml="1" mt="1" color="gray.300">Exemplo: nome@email.com</Text> }
+            </Box>
+            <Input 
+              placeholder="Digite sua Senha"
+              type="password"
+              label="Senha"
+              error={ errors.password }
+              icon={ FaLock }
+              { ...register("password") } 
+            />
+          </VStack>
+          <VStack marginTop="4" spacing="5">
+            <Button 
+                isLoading={ loading }
+                type="submit"
+                bg="purple.800"
+                w="100%"
+                h="50px"
+                color="white"
+                borderRadius="8px"
+                _hover={{
+                  bg: "purple.900"
+                }}
+              >Entrar</Button>
+              <Text color="gray.400">Ainda não possui uma conta ? </Text>
+              <Button 
+                isLoading={ loading }
+                type="button"
+                bg="gray.100"
+                w="100%"
+                h="50px"
+                color="gray.300"
+                borderRadius="8px"
+                _hover={{
+                  bg: "gray.200"
+                }}
+              >Entrar</Button>
           </VStack>
         </Grid>
       </Flex>
